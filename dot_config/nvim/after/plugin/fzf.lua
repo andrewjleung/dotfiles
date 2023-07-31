550b5c8d8aa5f174d60a1d80c88f7b5ea874dcf6
Andrew Leung <andrewleung104@gmail.com>
2023-07-30 22:49:23 -0500

    Use CR after fzf grep commands

--- dot_config/nvim/after/plugin/fzf.lua
+++ dot_config/nvim/after/plugin/fzf.lua
@@ -1,6 +1,5 @@
 local fzf = require('fzf-lua')
 
 vim.keymap.set("n", "<leader>ff", fzf.files)
-vim.keymap.set("n", "<leader>fg", fzf.grep)
-vim.keymap.set("n", "<leader>fG", fzf.grep_last)
-
+vim.keymap.set("n", "<leader>fg", ":FzfLua grep<CR><CR>")
+vim.keymap.set("n", "<leader>fG", ":FzfLua grep_last<CR>")
