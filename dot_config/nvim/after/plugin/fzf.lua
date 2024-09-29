0e1240b93de4e30e4a1a34790466744ed296deb0
Andrew Leung <andrewleung104@gmail.com>
2024-09-29 17:25:05 -0500

    Change fzf grep key to l

--- dot_config/nvim/after/plugin/fzf.lua
+++ dot_config/nvim/after/plugin/fzf.lua
@@ -1,8 +1,3 @@
-vim.keymap.set("n", "<leader>ff",
-  "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
-
-vim.keymap.set("n", "<leader>fg",
-  "<cmd>lua require('fzf-lua').live_grep_native()<CR>", { silent = true })
-
-vim.keymap.set("n", "<leader>fgl",
-  "<cmd>lua require('fzf-lua').live_grep_resume()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>l", "<cmd>lua require('fzf-lua').live_grep_native()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>ll", "<cmd>lua require('fzf-lua').live_grep_resume()<CR>", { silent = true })
