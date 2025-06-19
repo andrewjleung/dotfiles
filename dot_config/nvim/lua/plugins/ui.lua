2a61ecd3b060e6bcbb4b11192aeb81b6b5624a2d
Andrew Leung <andrewleung104@gmail.com>
2025-06-18 22:43:51 -0500

    Better diagnostics; ufo

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -27,4 +27,34 @@ return {
       },
     },
   },
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy", -- Or `LspAttach`
+    priority = 1000, -- needs to be loaded in first
+    opts = {
+      preset = "classic",
+    },
+  },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    config = function()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      }
+      local language_servers = vim.lsp.get_clients() -- or list servers manually like {'gopls', 'clangd'}
+      for _, ls in ipairs(language_servers) do
+        require("lspconfig")[ls].setup({
+          capabilities = capabilities,
+          -- you can add other fields for setting up lsp server in this table
+        })
+      end
+      require("ufo").setup()
+    end,
+    keys = {
+      { "<leader>z", "za", "Toggle fold under cursor (alias 'za')" },
+    },
+  },
 }
