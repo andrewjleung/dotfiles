d12c02533ee53f285861ede42a1c804dc7bd1a90
Andrew Leung <andrewleung104@gmail.com>
2025-08-09 13:33:39 -0500

    Absolutely scuffed dispatch p command

--- dot_config/nvim/lua/plugins/tools.lua
+++ dot_config/nvim/lua/plugins/tools.lua
@@ -2,11 +2,50 @@ return {
   "tpope/vim-dispatch",
   keys = {
     {
-      "<leader>rl",
+      "<leader>rr",
       function()
-        vim.cmd.Dispatch(vim.api.nvim_get_current_line():gsub("(?:^%s+|%s+$)", ""))
+        local current_line_num = vim.fn.line(".") - 1
+        local start_line_num = current_line_num
+        local end_line_num = current_line_num
+        local bufnr = vim.api.nvim_get_current_buf()
+
+        while start_line_num >= 0 do
+          local line = vim.api.nvim_buf_get_lines(bufnr, start_line_num, start_line_num + 1, false)[1]
+
+          if line == "" then
+            start_line_num = start_line_num + 1
+            break
+          end
+
+          if start_line_num < 1 then
+            break
+          end
+
+          start_line_num = start_line_num - 1
+        end
+
+        local line_count = vim.api.nvim_buf_line_count(bufnr)
+        while end_line_num < line_count do
+          local line = vim.api.nvim_buf_get_lines(bufnr, end_line_num, end_line_num + 1, false)[1]
+
+          if line == "" then
+            break
+          end
+
+          end_line_num = end_line_num + 1
+        end
+
+        local lines = vim.api.nvim_buf_get_lines(vim.api.nvim_get_current_buf(), start_line_num, end_line_num, false)
+        local paragraph = table.concat(lines, "\n")
+
+        if paragraph == "" then
+          vim.notify("Nothing to dispatch!", vim.log.levels.INFO)
+          return
+        end
+
+        vim.cmd.Dispatch(paragraph)
       end,
-      desc = "Run the current line (dispatch)",
+      desc = "Run the current paragraph (dispatch)",
     },
   },
 }
