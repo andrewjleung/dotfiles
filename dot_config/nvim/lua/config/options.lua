99833c0aeed11c0a9617c676e75af6c4cb107fbe
Andrew Leung <andrewleung104@gmail.com>
2025-08-12 23:39:29 -0500

    Ghostty changes; fix zshrc order

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -8,6 +8,8 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
+vim.opt.clipboard = ""
+
 vim.filetype.add({
   extension = {
     typ = "typst",
