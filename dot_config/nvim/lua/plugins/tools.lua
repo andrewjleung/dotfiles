fd1af6624ace5859adfbea6cbfdb9d2877d0af10
Andrew Leung <andrewleung104@gmail.com>
2025-10-05 22:42:30 -0500

    Add markdown TOC

--- dot_config/nvim/lua/plugins/tools.lua
+++ dot_config/nvim/lua/plugins/tools.lua
@@ -114,4 +114,15 @@ return {
       require("wrapping").setup()
     end,
   },
+  {
+    "hedyhli/markdown-toc.nvim",
+    ft = "markdown", -- Lazy load on markdown filetype
+    cmd = { "Mtoc" }, -- Or, lazy load on "Mtoc" command
+    opts = {
+      -- Your configuration here (optional)
+    },
+    config = function()
+      require("mtoc").setup({})
+    end,
+  },
 }
