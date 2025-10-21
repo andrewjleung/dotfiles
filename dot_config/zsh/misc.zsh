51cc767f67e93aa7c7ab558517be4ba8d94a5f6c
Andrew Leung <andrewleung104@gmail.com>
2025-10-21 16:31:20 -0500

    Buncha conf

--- dot_config/zsh/misc.zsh
+++ dot_config/zsh/misc.zsh
@@ -1,13 +1,24 @@
+alias bathelp='bat --plain --language=help'
+help() {
+  "$@" --help 2>&1 | bathelp
+}
+
 inspect_path() {
   # shellcheck disable=SC2001
   echo "$PATH" | sed 's/:/\n/g'
 }
 
 saul() {
+  chezmoi apply
+  tmux source ~/.config/tmux/tmux.conf
+  source ~/.zshrc
+}
+
+saulr() {
   local j="$(jobs)"
 
   if [[ -n "$j" ]]; then
-    print "background jobs detected:\n\n$j\n\nplease exit them before saul-ing"
+    print "background jobs detected:\n\n$j\n\nplease exit them before saulr-ing (this will run 'omz reload')"
     return 1
   fi
 
