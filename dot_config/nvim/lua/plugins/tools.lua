486f7207c38064a4eb22497f8967f368b9b1196b
Andrew Leung <andrewleung104@gmail.com>
2025-10-24 03:26:57 -0500

    Add reze conf

--- dot_config/nvim/lua/plugins/tools.lua
+++ dot_config/nvim/lua/plugins/tools.lua
@@ -19,57 +19,6 @@ return {
     end,
   },
   {
-    "tpope/vim-dispatch",
-    keys = {
-      {
-        "<leader>rr",
-        function()
-          local current_line_num = vim.fn.line(".") - 1
-          local start_line_num = current_line_num
-          local end_line_num = current_line_num
-          local bufnr = vim.api.nvim_get_current_buf()
-
-          while start_line_num >= 0 do
-            local line = vim.api.nvim_buf_get_lines(bufnr, start_line_num, start_line_num + 1, false)[1]
-
-            if line == "" then
-              start_line_num = start_line_num + 1
-              break
-            end
-
-            if start_line_num < 1 then
-              break
-            end
-
-            start_line_num = start_line_num - 1
-          end
-
-          local line_count = vim.api.nvim_buf_line_count(bufnr)
-          while end_line_num < line_count do
-            local line = vim.api.nvim_buf_get_lines(bufnr, end_line_num, end_line_num + 1, false)[1]
-
-            if line == "" then
-              break
-            end
-
-            end_line_num = end_line_num + 1
-          end
-
-          local lines = vim.api.nvim_buf_get_lines(vim.api.nvim_get_current_buf(), start_line_num, end_line_num, false)
-          local paragraph = table.concat(lines, "\n")
-
-          if paragraph == "" then
-            vim.notify("Nothing to dispatch!", vim.log.levels.INFO)
-            return
-          end
-
-          vim.cmd.Dispatch(paragraph)
-        end,
-        desc = "Run the current paragraph (dispatch)",
-      },
-    },
-  },
-  {
     "Wansmer/treesj",
     keys = {
       { "<leader>tt", "<cmd>TSJToggle<cr>", desc = "Toggle split/join for node under cursor" },
@@ -102,7 +51,10 @@ return {
   {
     "OXY2DEV/markview.nvim",
     lazy = false,
-    dependencies = { "loctvl842/monokai-pro.nvim", "saghen/blink.cmp" },
+    dependencies = {
+      "loctvl842/monokai-pro.nvim",
+      "saghen/blink.cmp",
+    },
   },
   {
     "MeanderingProgrammer/render-markdown.nvim",
