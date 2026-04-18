6cca3774ad48a9bb614c4a45a087d8906d6b6b9d
Andrew Leung <andrewleung104@gmail.com>
2026-04-18 17:01:56 -0500

    Setup zmx

--- dot_config/zsh/completions.zsh
+++ dot_config/zsh/completions.zsh
@@ -1,12 +1,12 @@
 if type mise > /dev/null; then
-    source <(mise completion zsh)
+    eval "$(mise completion zsh)"
 fi
 
 
 if type chezmoi > /dev/null; then
-    source <(chezmoi completion zsh)
+    eval "$(chezmoi completion zsh)"
 fi
 
 if type zmx > /dev/null; then
-    source <(zmx completions zsh)
+    eval "$(zmx completions zsh)"
 fi
